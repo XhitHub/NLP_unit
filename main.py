0ade7f083409ea1a27d409db002a1993d073b558
XhitHub <com.trojan.exe@gmail.com>
2023-11-08 12:19:04 +0800

    text_type_classifier

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import app_repl as app
 import _tests.t2 as appTest
+import text_type_classifier
 
 
-app.run()
+# app.run()
 # appTest.run()
+
+text_type_classifier.run('t4')
--- text_type_classifier.py
+++ text_type_classifier.py
@@ -5,22 +5,31 @@ import nlp.nlp_controller as nlpc
 inputFolder = 'files/inputs'
 outputFolder = 'files/outputs'
 
-project = 't3'
-inFilename = inputFolder + '/' + project + '.txt'
-outProjectFolder = outputFolder + '/' + project
+# project = 't4'
 
-def run():
+def run(project):
+  inFilename = inputFolder + '/' + project + '.txt'
+  outProjectFolder = outputFolder + '/' + project
   text = io.textFileToString(inFilename)
   sents = nlpc.textToSents(text)
   nlpDicts = []
   nlpTypesDict = {}
   for sent in sents:
-    if sent != '':
-      sent = sent.replace('\n','')
+    if not sent.text == '':
+      print(sent)
+      # sent = sent.replace('\n','')
       nlpDict = nlpc.sentToNLPSequence(sent)
+      nlpDict['_text'] = nlpDict['_text'].replace('\n', '')
+      nlpDict['lemmaSeq'] = nlpDict['lemmaSeq'].replace('\n', '')
       nlpDicts.append(nlpDict)
-      pos = nlpDict[posSeq]
+      pos = nlpDict['posSeq']
       if pos not in nlpTypesDict:
         nlpTypesDict[pos] = {
-          
+          'count': 0
         }
+      nlpTypesDict[pos]['count'] = nlpTypesDict[pos]['count'] + 1
+  print(nlpDicts)
+  for nd in nlpDicts:
+    nd['count'] = nlpTypesDict[nd['posSeq']]['count']
+  nlpDicts.sort(key=lambda x: x['count'], reverse=True)
+  io.mapListToCsv(outProjectFolder + '/nlp_seqs.csv', nlpDicts)  
